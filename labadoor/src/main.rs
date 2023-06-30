1eef68f859fd4c566ccb9f0035cc1cf2a92d7a61
George Kaklamanos <gkaklas@gkaklas.gr>
2023-06-30 17:23:05 +0300

    Add crate features to select used functionality

--- labadoor/src/main.rs
+++ labadoor/src/main.rs
@@ -8,24 +8,32 @@ struct Cli {
 
 #[derive(Parser, Debug)]
 enum Command {
+    #[cfg(feature = "telegram")]
     Telegram,
+    #[cfg(feature = "matrix")]
     Matrix,
+    #[cfg(feature = "csv")]
     CSV,
+    #[cfg(feature = "gpio")]
     GPIO,
 }
 
 fn main() {
     let cli = Cli::parse();
     match &cli.command {
+        #[cfg(feature = "telegram")]
         Command::Telegram => {
             labadoor_telegram::main();
         }
+        #[cfg(feature = "matrix")]
         Command::Matrix => {
             labadoor_matrix::main();
         }
+        #[cfg(feature = "csv")]
         Command::CSV => {
             labadoor_csv::main();
         }
+        #[cfg(feature = "gpio")]
         Command::GPIO => {
             labadoor_gpio::main();
         }
